200eec79cb7e8f4e1ab325658ae0d870bad41c20
KonevDenis <konevd433@gmail.com>
2022-10-06 18:44:14 +0300

    разобрался с перегонкой в int из str программа вроде работает

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 print("Hello World")
 jojo = open("number.txt", "r")
 number = jojo.readline()
+print(number)
 spisok = number.split()
+spisok1 = []
 print(spisok)
+for a in spisok:
+    spisok1.append(int(a))
+print(spisok1)
 n = len(spisok)
 print(n)
 for p in range(0, n):
     for i in range (0, n-1):
-        if spisok[i] > spisok[i+1]:
-            spisok[i], spisok[i+1] = spisok[i+1], spisok[i]
-    print (spisok)
+        if spisok1[i] > spisok1[i+1]:
+            spisok1[i], spisok1[i+1] = spisok1[i+1], spisok1[i]
+    print (spisok1)
 
